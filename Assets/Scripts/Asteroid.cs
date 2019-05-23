b32ddfc722f3199be1f0ef0b0855ae2f92c0717d
ChaitanyaDeo <chaitanya8514@gmail.com>
2019-05-23 20:05:05 +0530

    compressed build.bc and deleted temp

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    // Sprites
+    [SerializeField]
+    Sprite AsteroidSprite1;
+    [SerializeField]
+    Sprite AsteroidSprite2;
+    [SerializeField]
+    Sprite AsteroidSprite3;
+
+    // random movement constants
+    const float MinImpulseForce = 0.5f;
+    const float MaxImpulseForce = 2f;
+
+    /// <summary>
+    /// Makes the asteroid fly in the given direction
+    /// </summary>
+    public void StartMoving(float angle)
+    {
+        Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+        float magnitude = Random.Range(MinImpulseForce, MaxImpulseForce);
+        GetComponent<Rigidbody2D>().AddForce(direction * magnitude, ForceMode2D.Impulse);
+    }
+
+    /// <summary>
+    /// Picks asteroid direction based on where it starts
+    /// </summary>
+    public void Initialize(Direction Dir, Vector3 Loc)
+    {
+        //set position
+        gameObject.transform.position = Loc;
+
+        // random movement code
+        float angle = Random.Range(0, 30 * Mathf.Deg2Rad);
+
+        switch (Dir)
+        {
+            case Direction.Right:
+                angle -= 15 * Mathf.Deg2Rad;
+                break;
+            case Direction.Up:
+                angle += 75 * Mathf.Deg2Rad;
+                break;
+            case Direction.Left:
+                angle += 165 * Mathf.Deg2Rad;
+                break;
+            case Direction.Down:
+                angle += 255 * Mathf.Deg2Rad;
+                break;
+            default:
+                break;
+        }
+
+
+        StartMoving(angle);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        // set a random sprite
+        switch (Random.Range(0, 2))
+        {
+            case 0:
+                gameObject.GetComponent<SpriteRenderer>().sprite = AsteroidSprite1;
+                break;
+            case 1:
+                gameObject.GetComponent<SpriteRenderer>().sprite = AsteroidSprite2;
+                break;
+            case 2:
+                gameObject.GetComponent<SpriteRenderer>().sprite = AsteroidSprite3;
+                break;
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Destroy asteroids on collision with bullets
+    /// </summary>
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Bullet")
+        {
+            AudioManager.Play(AudioClipName.AsteroidHit);
+            Destroy(coll.gameObject);
+
+            // if the asteroid isn't small enough - spawn 2 smaller asteroids first
+            if (!(gameObject.transform.localScale.x < 0.5f))
+            {
+                Vector3 newScale = gameObject.transform.localScale;
+                newScale.x /= 2;
+                newScale.y /= 2;
+                gameObject.transform.localScale = newScale;
+                gameObject.GetComponent<CircleCollider2D>().radius /= 2;
+
+
+                GameObject Debree1 = Instantiate<GameObject>(gameObject);
+                Debree1.GetComponent<Asteroid>().StartMoving(Random.Range(0, 2 * Mathf.PI));
+                GameObject Debree2 = Instantiate<GameObject>(gameObject);
+                Debree2.GetComponent<Asteroid>().StartMoving(Random.Range(0, 2 * Mathf.PI));
+            }
+
+            Destroy(gameObject);
+
+        }
+    }
+}
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    [SerializeField]
+    GameObject BulletPrefab;
+
+    [SerializeField]
+    Canvas UIref;
+    // thrust and rotation variables
+    Rigidbody2D rigb;
+    Vector2 thrustDirection = new Vector2(1, 0);
+
+    const float ThrustForce = 10;
+    const float RotateDPS = 270;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // rotation with changing thrust direction
+        float rotationInput = Input.GetAxis("Rotate");
+        if (rotationInput != 0)
+        {
+
+            float rotationAmnt = RotateDPS * Time.deltaTime;
+
+            if (rotationInput < 0)
+            {
+                rotationAmnt *= -1;
+            }
+            transform.Rotate(Vector3.forward, rotationAmnt);
+
+            float zRotation = transform.eulerAngles.z * Mathf.Deg2Rad;
+            thrustDirection.x = Mathf.Cos(zRotation);
+            thrustDirection.y = Mathf.Sin(zRotation);
+
+        }
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            GameObject BulletObj = Instantiate<GameObject>(BulletPrefab);
+            BulletObj.transform.SetPositionAndRotation(gameObject.transform.position, gameObject.transform.rotation);
+            BulletObj.GetComponent<Bullet>().ApplyForce(thrustDirection);
+
+            AudioManager.Play(AudioClipName.PlayerShot);
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+        if (Input.GetAxis("Thrust") != 0)
+        {
+            rigb.AddForce(ThrustForce * thrustDirection, ForceMode2D.Force);
+        }
+
+    }
+
+    /// <summary>
+    /// OnCollisionEnter2D where we explode 
+    /// if the ship collides with an asteriod
+    /// </summary>
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Asteroid")
+        {
+            UIref.GetComponent<HUD>().stopGameTimer();
+            AudioManager.Play(AudioClipName.PlayerDeath);
+            Destroy(gameObject);
+        }
+
+    }
+}
